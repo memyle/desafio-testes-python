5d236239d1aaf62dc70b8f1ee7d5f719ba94388d
Memyle <emily.tomas@ufms.br>
2023-06-14 15:01:27 -0400

    feat: arquivos executaveis para teste

---
+++ testes.py
@@ -0,0 +1,9 @@
+import sys
+import subprocess
+with open("entradasteste.txt", "r",encoding="utf-8") as test:
+    contador = 0
+    for linha in test.readlines():
+        contador+=1
+        print ("\nCenário " + str(contador))
+        print ("Comando: " + str(linha))
+        subprocess.run(linha, shell=True)
